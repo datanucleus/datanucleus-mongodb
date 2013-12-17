77e62796a88b7fb0f6407d7081828ee64be2f5d6
andyjefferson <andy@datanucleus.org>
2013-12-17 13:51:08 +0000

    StringBuffer -> StringBuilder

--- src/java/org/datanucleus/store/mongodb/MongoDBPersistenceHandler.java
+++ src/java/org/datanucleus/store/mongodb/MongoDBPersistenceHandler.java
@@ -442,7 +442,7 @@ public class MongoDBPersistenceHandler extends AbstractPersistenceHandler
             AbstractClassMetaData cmd = op.getClassMetaData();
             if (NucleusLogger.DATASTORE_PERSIST.isDebugEnabled())
             {
-                StringBuffer fieldStr = new StringBuffer();
+                StringBuilder fieldStr = new StringBuilder();
                 for (int i=0;i<fieldNumbers.length;i++)
                 {
                     if (i > 0)
@@ -630,7 +630,7 @@ public class MongoDBPersistenceHandler extends AbstractPersistenceHandler
             if (NucleusLogger.DATASTORE_RETRIEVE.isDebugEnabled())
             {
                 // Debug information about what we are retrieving
-                StringBuffer str = new StringBuffer("Fetching object \"");
+                StringBuilder str = new StringBuilder("Fetching object \"");
                 str.append(op.getObjectAsPrintable()).append("\" (id=");
                 str.append(op.getInternalObjectId()).append(")").append(" fields [");
                 for (int i=0;i<fieldNumbers.length;i++)
