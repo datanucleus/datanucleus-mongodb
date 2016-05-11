47ab8fb17da36635604332032c230bf99cdc7b41
andyjefferson <andy@datanucleus.org>
2016-05-11 18:03:07 +0100

    Supported query options

--- src/main/java/org/datanucleus/store/mongodb/MongoDBStoreManager.java
+++ src/main/java/org/datanucleus/store/mongodb/MongoDBStoreManager.java
@@ -93,6 +93,8 @@ public class MongoDBStoreManager extends AbstractStoreManager implements SchemaA
         set.add(StoreManager.OPTION_ORM_SERIALISED_PC);
         set.add(StoreManager.OPTION_TXN_ISOLATION_READ_COMMITTED);
         set.add(StoreManager.OPTION_DATASTORE_TIME_STORES_MILLISECS);
+        set.add(StoreManager.OPTION_QUERY_JDOQL_BULK_DELETE);
+        set.add(StoreManager.OPTION_QUERY_JPQL_BULK_DELETE);
         return set;
     }
 
