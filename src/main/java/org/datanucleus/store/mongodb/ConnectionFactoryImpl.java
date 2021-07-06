d9e5b33af4b35ecaf2cf924fa19304aabfc8f95c
Raymond Nathan <rayman_sticky@live.com>
2021-07-06 17:52:05 +0800

    Updates based on https://github.com/datanucleus/datanucleus-mongodb/pull/58#issuecomment-874600735

--- src/main/java/org/datanucleus/store/mongodb/ConnectionFactoryImpl.java
+++ src/main/java/org/datanucleus/store/mongodb/ConnectionFactoryImpl.java
@@ -4,7 +4,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-http://www.apache.org/licenses/LICENSE-2.0
+    http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,11 +13,14 @@ See the License for the specific language governing permissions and
 limitations under the License.
 
 Contributors:
-...
+    ...
 **********************************************************************/
 package org.datanucleus.store.mongodb;
 
-import com.mongodb.*;
+import com.mongodb.DB;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientOptions;
+import com.mongodb.MongoClientURI;
 import org.datanucleus.ExecutionContext;
 import org.datanucleus.PropertyNames;
 import org.datanucleus.exceptions.NucleusException;
